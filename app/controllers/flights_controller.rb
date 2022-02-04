48d8fac416e5b606fb08d33aa83bcfd9cfb567f9
brianjrok <brianjrok@gmail.com>
2022-02-03 23:02:15 -0500

    search form with table showing flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,11 @@
 class FlightsController < ApplicationController
     def index
-        @flights = Flight.all
+        puts :flight.to_s
+        @airports = Airport.all.map{ |a| [a.airport_city, a.id ] }
+        @flights = Flight.where(flight_params).includes(:departure_airport, :arrival_airport)
+    end
+
+    def flight_params
+        params.require(:flight).permit(:departure, :arrival)
     end
 end
