17ab946da3e229431e42fa25170157e6152f6674
brianjrok <brianjrok@gmail.com>
2022-02-07 17:55:17 -0500

    set up booking mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+    default from: 'fakeflight@odinflightbooker.com'
+
+    def confirmation_email
+        @booking = Booking.find(params[:booking_id])
+        @url = booking_url(@booking)
+        mail(to: @booking.passengers.pluck(email), subject: "You have booked Flight #{@booking.flight.id} from 
+            #{@booking.flight.departure_airport.airport_city} to #{@booking.flight.arrival_airport.airport_city}!")
+    end
 end
